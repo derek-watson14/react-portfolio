190c6b001499a6425997d9a2fe7cc13ceed55d9d
Derek Watson <derek.watson92@gmail.com>
2020-11-29 00:47:31 -0800

    updated projects and other data

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,14 +4,14 @@ import { nanoid } from 'nanoid';
 export const headData = {
   title: 'Derek Watson | Developer', // e.g: 'Name | Developer'
   lang: 'en', // e.g: en, es, fr, jp
-  description: 'Web Development portfolio for Derek Watson!', // e.g: Welcome to my website
+  description: 'Web Development portfolio for Derek Watson', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: 'Hey there! My name is',
   name: 'Derek',
-  subtitle: 'and I like to make stuff with code.',
+  subtitle: 'and I like to build modern websites.',
   cta: 'About me',
 };
 
@@ -21,10 +21,10 @@ export const aboutData = {
   paragraphOne:
     "I'm Derek Watson, a Seattle-based web developer searching for professional opportunities. After almost two years of online computer science and web development courses, in September 2020 I comleted the University of Washington's full stack web development bootcamp.",
   paragraphTwo:
-    "My skillset focuses on web technologies. I am proficient with HTML, CSS/SASS and JavaScript (including Node.js, Express, React and jQuery) and am familiar with SQL, MongoDB and Python (Flask). I'm constantly seeking to expand my abilites - be that through learning new technologies, writing cleaner code or deepening my understanding of languages I've worked with in the past.",
+    "My skillset focuses on web technologies. I am proficient with HTML, CSS/SASS and JavaScript (including Node.js, Express, React and jQuery) and am familiar with SQL, MongoDB and GraphQL. I'm constantly seeking to expand my abilites - be that through learning new technologies, writing cleaner code or deepening my understanding of languages I've worked with in the past.",
   paragraphThree:
     'Outside of programming, I studied Strategic Communication at Washington State University, spent five years travelling internationally, know German at an advanced-intermediate level and run 20-40km a week.',
-  resume: 'https://docs.google.com/document/d/1Inc9zC3Ch67CiwF_n4cEsaZYwJrlquuI_tPq0RdZ0xM/edit',
+  resume: 'https://docs.google.com/document/d/1Inc9zC3Ch67CiwF_n4cEsaZYwJrlquuI_tPq0RdZ0xM',
 };
 
 // PROJECTS DATA
@@ -42,14 +42,14 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'derdiedas.png',
-    title: 'DerDieDas: practice your German',
+    img: 'organic-gold.png',
+    title: 'Organic Gold Music: band & recording Studio',
     info:
-      'I created DerDieDas in early 2019 when I was living in Germany and first learning both German and programming. Learning things like German noun genders and preposition cases was challenging, so I hoped to put my new programming skills to the test and build a study application.',
+      'Organic Gold is a band and new recording studio based on Bainbridge Island, WA. They needed a website where they could showcase their music, communicate about events and recieve studio requests.',
     info2:
-      'DerDieDas was built with Flask (Python) and PostgreSQL on the back end and normal HTML, CSS and Javascript on the front. As I built the entire thing with only Google for assistance, it remains one of the projects I am most proud of.',
-    url: 'https://deutsch-grammatik.herokuapp.com/about',
-    repo: 'https://github.com/derek-watson14/DerDieDas',
+      'This is the first site I was paid for and was built with Gatsby (React) and Sanity.io for a headless CMS. The data layer uses GraphQL and contact emails are sent via a serverless function.',
+    url: 'https://organicgoldmusic.com/',
+    repo: 'https://github.com/derek-watson14/organic-gold',
   },
   {
     id: nanoid(),
@@ -64,24 +64,14 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'drinkme.png',
-    title: 'drinkMe: find cocktail recipes and bars',
+    img: 'derdiedas.png',
+    title: 'DerDieDas: practice your German',
     info:
-      'drinkME was the first group project for the UW full stack coding boot camp, and was my first experience using git with multiple contributors. I was mostly responsible for the page design, which used Materialize CSS, and I also figured out the Google Maps portion of the site.',
-    info2: 'This project was built with HTML, Materialize CSS, JavaScript and jQuery.',
-    url: 'https://derek-watson14.github.io/drinkME/',
-    repo: 'https://github.com/derek-watson14/drinkME',
-  },
-  {
-    id: nanoid(),
-    img: 'hachikuchi.png',
-    title: 'Hachi Kuchi: artist collective',
-    info:
-      "Hachi Kuchi is the website I made for my friend's artist collective, which was becoming quite active pre-Covid. Since then events have been cancelled and the site is in need of an information update.",
+      'I created DerDieDas in early 2019 when I was living in Germany and first learning both German and programming. Learning things like German noun genders and preposition cases was challenging, so I hoped to put my new programming skills to the test and build a study application.',
     info2:
-      'The site was built using Gatsby (React) with styled components and was deployed on Netlify.',
-    url: 'https://hachikuchi.com/',
-    repo: 'https://github.com/derek-watson14/hachi-kuchi',
+      'DerDieDas was built with Flask (Python) and PostgreSQL on the back end and normal HTML, CSS and Javascript on the front. As I built the entire thing with only Google for assistance, it remains one of the projects I am most proud of.',
+    url: 'https://deutsch-grammatik.herokuapp.com/about',
+    repo: 'https://github.com/derek-watson14/DerDieDas',
   },
 ];
 
